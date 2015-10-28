c9d080bdeb19b86e87d26127e303e343550bf40e
Arjun Venkataswamy <avenkat2@gmail.com>
2015-10-28 10:01:07 -0500

    adds respond_to block

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,19 @@
 class MoviesController < ApplicationController
   def index
     @movies = Movie.all
+
+    respond_to do |format|
+      format.html { render 'index'}
+      format.json { render json: @movies }
+    end
   end
 
   def show
     @movie = Movie.find(params[:id])
+
+    respond_to do |format|
+      format.html { render 'show'}
+      format.json { render json: @movie }
+    end
   end
 end
